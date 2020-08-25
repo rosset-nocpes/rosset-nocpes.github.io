535de2075e30286eac7a8bd249a5e9db78cf83a0
CoderPY4 <coderpy4@users.noreply.github.com>
2020-08-25 20:00:59 +0300

    Update

---
+++ js/init.js
@@ -0,0 +1,14 @@
+//Hook up the tweet display
+
+$(document).ready(function() {
+						   
+	$(".countdown").countdown({
+				date: "27 aug 2020 18:30:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
