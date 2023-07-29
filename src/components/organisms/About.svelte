136fc9f572a456285c5ad7c1a4cc851f037b0da2
Andrii Blacksmith <coderpy4@proton.me>
2023-07-30 01:07:30 +0300

    Initial
    Signed-off-by: Andrii Blacksmith <coderpy4@proton.me>

---
+++ src/components/organisms/About.svelte
@@ -0,0 +1,96 @@
+<script>
+	import RichPresence from '../molecules/RichPresence.svelte';
+	import Tooltip from '../atoms/Tooltip.svelte';
+
+	let getAge = () => {
+		let birthDate = new Date('2008/08/30');
+		const ageMs = Date.now() - birthDate.getTime();
+		const preciseAge = (ageMs / 31536000000).toFixed(10);
+		return preciseAge;
+	};
+
+	let age = getAge();
+	setInterval(() => {
+		age = getAge();
+	}, 1000);
+</script>
+
+<section id="about" class="wrapper">
+	<div>
+		<RichPresence />
+	</div>
+	<div class="text">
+		<h2>bio</h2>
+		<p>
+			Hey there, I'm ~rosset-nocpes! :] I'm a <Tooltip tip={age}
+				><span>{Math.floor(Number(age))}</span></Tooltip
+			>
+			year old developer and graphic designer based in
+			<Tooltip tip="🇺🇦">
+				<span>Ukraine</span>
+			</Tooltip>
+			. I’ve taken programming seriously since
+			<span>2018</span>, and have been using fully Linux since <span>2023</span>.
+		</p>
+	</div>
+</section>
+
+<style lang="scss">
+	@import '../../styles/mixins.scss';
+
+	section {
+		margin-bottom: 6rem;
+		display: grid;
+		gap: 4.5rem;
+		grid-template-columns: 1fr 1fr;
+		align-items: center;
+	}
+
+	.text {
+		position: relative;
+		line-height: 1.75rem;
+	}
+
+	span {
+		font-weight: 400;
+		font-family: var(--font-two);
+		font-size: 0.9rem;
+		background-color: var(--neutral-one);
+		border-radius: 7px;
+		color: var(--yellow);
+		padding: 0.2rem 0.5rem 0.2rem;
+		width: fit-content;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	.text::before {
+		@include outlineText(
+			$content: 'r.s',
+			$translateX: 97%,
+			$translateY: -5%,
+			$fontSize: 300px,
+			$opacity: 0.22
+		);
+	}
+
+	h2 {
+		display: none;
+		margin-top: 1rem;
+	}
+
+	@media (max-width: 868px) {
+		section {
+			display: flex;
+			flex-direction: column;
+			align-items: normal;
+		}
+
+		h2 {
+			display: block;
+			margin-bottom: 1rem;
+		}
+	}
+</style>
---
+++ src/util/discord.ts
@@ -0,0 +1,8 @@
+class DiscordUser {
+    constructor( 
+      public username: string, 
+      public id: string 
+    ) {}
+  }
+  
+  export let user = new DiscordUser("rosset.nocpes", '416254813454467073');
